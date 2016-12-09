822916b2cba4a4a7526ffc68fc6c4ecae8f96620
ulises <ulises@ulises-TravelMate-P253>
2016-12-09 20:14:59 +0100

    First commit

---
+++ src/main/java/kodea/O2SMS2/App.java
@@ -0,0 +1,13 @@
+package kodea.O2SMS2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
